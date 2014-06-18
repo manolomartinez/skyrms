5eb22891b5192f9a18cbdd2533b2fd8f1ab43b4c
Manolo Martínez <manolo@austrohungaro.com>
2014-06-18 14:50:23 +0200

    Using nim.coarse_investment() for the prepare table, amd cleanup

--- Dynamic/pokemons.py
+++ Dynamic/pokemons.py
@@ -2,7 +2,10 @@
 Library for the paper "Signalling games and modality"
 """
 
+import nim
 import setup as s
+
+import numpy as np
 
 
 class MainGame(s.Strategies):
@@ -11,54 +14,65 @@ class MainGame(s.Strategies):
     """
 
     def __init__(self):
-        preparetable = [[2, i, j] for i in range(3) for j in range(3-i)]
-        acts = [[0], [1]] + preparetable
+        investments = nim.coarse_investments(1, 2, [0, 0.25, 0.5, 0.75, 1])
+        preparetable = np.insert(investments, 0, 2, axis=1)
+        attacks = np.array([[0, 0, 0], [1, 0, 0]])
+        acts = np.concatenate((attacks, preparetable))
         s.Strategies.__init__(self, 3, 3, acts)
-        self.unpair = 0
-        self.unpsea = 0
-        self.loseair = 4
-        self.losesea = 4
+        self.unpair = 0  # the unprepared air attack is equivalent investing
+        # this amount in preparation
+        self.unpsea = 0  # ditto for the unprepared sea attack
+        self.loseair = 0  # the payoff of losing against the air monster
+        self.losesea = 0  # ditto for the sea monster
+        self.probs = [0.2, 0.2, 0.3, 0.3]  # The probs of air monster, sea
+        self.probair = self.probs[0] / (self.probs[0] + self.probs[1])
+        self.probsea = self.probs[1] / (self.probs[0] + self.probs[1])
+        self.probtoair = self.probs[2] / (self.probs[2] + self.probs[3])
+        self.probtosea = self.probs[3] / (self.probs[2] + self.probs[3])
+        # monster, undefined to air monster and to sea monster
 
-    def payoff1stround(self):
+    def payoff1stround(self, cost):
         """
         Calculate the payoff matrix for the first round
         """
-        return [[pay(self.unpair), self.loseair, self.loseair -
-                 self.costair], [self.losesea, pay(self.unpsea),
-                                 self.losesea - self.costsea],
-                [(self.losesea + pay(self.unpair)) / 2,
-                 (self.loseair + pay(self.unpsea))/2, 0]]
+        return [[pay(self.unpair), self.loseair, self.loseair - cost],
+                [self.losesea, pay(self.unpsea), self.losesea - cost],
+                [self.probsea * self.losesea + self.probair * pay(self.unpair),
+                 self.probair * self.loseair + self.probsea * pay(self.unpsea),
+                 0]]
 
-    def payoff2ndround(self):
+    def payoff2ndround(self, costair, costsea, cost):
         """
         Calculate the payoff matrix for the second round
         """
-        gain = [[pay(self.costair), self.loseair, self.loseair],
-                [self.losesea, pay(self.costsea), self.losesea]]
-        return [[member - self.costair - self.costsea
-                 for member in row] for row in gain]
+        gain = np.array([[pay(costair), self.loseair, self.loseair],
+                         [self.losesea, pay(costsea), self.losesea]])
+        return gain - cost
 
     def payoff(self, sender, receiver, initialstate, secondstate):
         """
         Calculate the payoff for certain sender and receiver strats, and
         certain first and second states
         """
-        firstmessage = self.senderstrategies[sender][initialstate]
-        firstresponse = self.receiverstrategies[receiver][firstmessage]
+        firstmessage = int(self.senderstrategies[sender][initialstate])
+        firstresponse = np.array(
+            self.receiverstrategies[receiver][firstmessage], np.int32)
         firstr = firstresponse[0]
-        try:
-            self.costair = firstresponse[1]
-            self.costsea = firstresponse[2]
-        except IndexError:
-            self.costair = 0
-            self.costsea = 0
-        if initialstate != 2 or len(firstresponse) == 1:
-            return self.payoff1stround()[initialstate][firstr]
+        if firstr == 2:
+            costair = firstresponse[1]
+            costsea = firstresponse[2]
+            cost = costair + costsea
         else:
-            secondmessage = self.senderstrategies[sender][secondstate]
-            secondresponse = self.receiverstrategies[receiver][secondmessage]
+            cost = 0
+        if initialstate != 2 or firstr != 2:
+            return self.payoff1stround(cost)[initialstate][firstr]
+        else:
+            secondmessage = int(self.senderstrategies[sender][secondstate])
+            secondresponse = np.array(
+                self.receiverstrategies[receiver][secondmessage], np.int32)
             secondr = secondresponse[0]
-            return self.payoff2ndround()[secondstate][secondr]
+            return self.payoff2ndround(
+                costair, costsea, cost)[secondstate][secondr]
 
     def avgpayoff(self, sender, receiver):
         """
@@ -68,12 +82,14 @@ class MainGame(s.Strategies):
         seamonster = self.payoff(sender, receiver, 1, 1)
         toairmonster = self.payoff(sender, receiver, 2, 0)
         toseamonster = self.payoff(sender, receiver, 2, 1)
-        return (1/6 * airmonster + 1/6 * seamonster + 2/3 * toairmonster + 2/3
-                * toseamonster)
+        avgpoff = (
+            1/6 * airmonster + 1/6 * seamonster + 2/3 * toairmonster + 2/3
+            * toseamonster)
+        return [avgpoff, avgpoff]
 
 
 def pay(cost):
     """
     A diminishing-returns pay function
     """
-    return 20 - 10/s.np.exp(cost)
+    return 5 * (1 + cost)
