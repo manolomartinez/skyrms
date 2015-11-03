edc6e7e7d0d38254a7d060ec65f4aa0ff7719c8d
Manolo Martínez <manolo@austrohungaro.com>
2015-11-03 13:46:54 +0100

    new package name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name='Signal',
+    name='skyrms',
     version='0.1',
     install_requires=['numpy', 'scipy'],
     description='Dynamic analysis of signaling games',
     author='Manolo Martínez',
     author_email='mail@manolomartinez.net',
     url='https://github.com/manolomartinez/signal',
-    packages=['signal'],
+    packages=['skyrms'],
     license='GPLv3'
 )
