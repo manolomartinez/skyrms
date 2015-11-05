fb9855171febef8e149e9c025062c522c63d72a9
Manolo Martínez <manolo@austrohungaro.com>
2015-11-05 12:04:56 +0100

    Making calculate (mostly) compatible with the new game.py

--- skyrms/calculate.py
+++ skyrms/calculate.py
@@ -1,72 +1,75 @@
 """
-Solve large batches of games
+Solve large batches of games. There are a bunch of idyosincratic functions
+here. This module is mostly for illustration of use cases.
 """
-import game
+import skyrms.game
 import json
 import os.path
 import pickle
 import multiprocessing
 import itertools as it
-
-# t = s.np.arange(1001)
-# strat = s.Strategies(2,2,2)
-# game = s.Game(
-#    [63, 67, 10, 19, 8, 4, 4, 31, 40, 35, 92,
-#     36, 47, 59, 62, 77, 24, 34], 0.01, strat)
+import skyrms.game.np as np
 
 
-def one_basin_mixed(game, trials):
+def one_basin_mixed(game, trials, times):
     """
-    Calculate evolutions for <trials> starting points
+    Calculate evolutions for <trials> starting points of <game> (which is an
+    instance of game.Evolve), in <times> (an instance of game.Times)
     """
     pool = multiprocessing.Pool(None)
     remain = trials
     # nash = s.Nash(game)
     newsols = pool.imap_unordered(one_basin_aux_mixed, zip(range(remain),
-                                  it.repeat(game)))
-    data = s.np.array([sol for sol in newsols])
+                                  it.repeat(game), it.repeat(times)))
+    data = np.array([sol for sol in newsols])
     pool.close()
     pool.join()
     return data
 
 
-def one_basin_aux_mixed(pair):
+def one_basin_aux_mixed(triple):
     """
     Calculate the one_basin loop. First odeint, then ode if error
     """
-    s.np.random.seed()
-    print("trial {} -- odeint".format(pair[0]))
-    data = one_run_odeint(pair[1], pair[1].strats.random_sender(),
-                          pair[1].strats.random_receiver())
+    print("trial {} -- odeint".format(triple[0]))
+    np.random.seed()
+    game = triple[1]
+    times = triple[2]
+    data = game.replicator_odeint(game.random_sender(), game.random_receiver(),
+                                  times)
     if test_failure(data[1]):
-        print("trial {} -- ode".format(pair[0]))
-        sols = one_run_ode(pair[1], pair[1].strats.random_sender(),
-                           pair[1].strats.random_receiver())
+        print("trial {} -- ode".format(triple[0]))
+        sols = game.replicator_ode(game.random_sender(),
+                                   game.random_receiver(), times)
     else:
         sols = data[0]
     tofile = [sols[0]] + [sols[-1]]
     return tofile
 
 
-def one_basin_aux(pair):
+def one_basin_aux(triple):
     """
-    Calculate the one_basin loop using one_run_odeint
+    Calculate the one_basin loop using replicator_odeint
     """
-    s.np.random.seed()
-    print("trial {}".format(pair[0]))
-    sols = one_run_odeint(pair[1], pair[1].strats.random_sender(),
-                          pair[1].strats.random_receiver())
+    np.random.seed()
+    print("trial {}".format(triple[0]))
+    game = triple[1]
+    times = triple[2]
+    sols = game.replicator_odeint(game.random_sender(), game.random_receiver(),
+                                  times)
     return sols
 
 
-def one_basin_ode_aux(pair):
+def one_basin_ode_aux(triple):
     """
     Calculate the one_basin loop using one_run_ode
     """
-    s.np.random.seed()
-    print("trial {}".format(pair[0]))
-    sols = one_run_ode(pair[1], pair[1].strats.random_sender(),
-                       pair[1].strats.random_receiver())
+    np.random.seed()
+    print("trial {}".format(triple[0]))
+    game = triple[1]
+    times = triple[2]
+    sols = game.replicator_ode(game.random_sender(), game.random_receiver(),
+                               times)
     return sols
 
 
@@ -104,8 +107,8 @@ def prob_vector(vector):
     """
     Test if <vector> is a probability vector
     """
-    return abs(s.np.sum(vector) - 1) < 1e-5 and s.np.all([0 <= elem for elem
-                                                          in vector])
+    return abs(np.sum(vector) - 1) < 1e-5 and np.all([0 <= elem for elem in
+                                                      vector])
 
 
 def test_success(element):
@@ -116,13 +119,13 @@ def test_failure(element):
     return "successful" not in element['message']
 
 
-vtest_success = s.np.vectorize(test_success)
-vtest_failure = s.np.vectorize(test_failure)
+vtest_success = np.vectorize(test_success)
+vtest_failure = np.vectorize(test_failure)
 
 
 def test_endstate(array):
     """
     Test if <array> is composed by two concatenated probability vectors
     """
-    vectors = s.np.split(s.np.around(array, decimals=10), 2)
+    vectors = np.split(np.around(array, decimals=10), 2)
     return prob_vector(vectors[0]) and prob_vector(vectors[1])
